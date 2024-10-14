efa6ec7c827b17ccca9f0c87b639d09a11eb8ceb
CorbinCoder <66671323+CorbinCoder@users.noreply.github.com>
2024-10-15 00:26:18 +1100

    changed app variable

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, app
-application = Flask(__name__)
+from flask import Flask
+app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'Hello World!'
 
 if __name__ == '__main__':
-    application.run()
+    app.run()
